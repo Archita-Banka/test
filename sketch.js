12808ffd05bbfbd2202f3fdb48924705f6f9f4a0
Archita Banka <architabanka15@gmail.com>
2020-11-28 12:46:39 +0530

    These changes are well suited

--- sketch.js
+++ sketch.js
@@ -18,14 +18,14 @@ function setup(){
     
     ground = new Ground(600,height,1200,20)
 
-    box1 = new Box(700,320,70,70);
+    box1 = new Box(70,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
-    box3 = new Box(700,240,70,70);
+    box3 = new Box(30,240,70,70);
     box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    pig3 = new Pig(910, 220);
 
     log3 =  new Log(810,180,300, PI/2);
 
